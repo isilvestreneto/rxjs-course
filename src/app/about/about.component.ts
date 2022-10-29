3ef750349b77cb74b0810d25ae61bbd14bcdade0
Ivan Silvestre <isilvestreneto@gmail.com>
2022-10-29 17:03:22 -0300

    imperative design

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -11,15 +11,5 @@ import { map } from "rxjs/operators";
 export class AboutComponent implements OnInit {
   constructor() {}
 
-  ngOnInit() {
-    const http$ = createHttpObservable("/api/courses");
-
-    const courses$ = http$.pipe(map((res) => Object.values(res["payload"])));
-
-    courses$.subscribe(
-      (courses) => console.log(courses),
-      noop,
-      () => console.log("completed")
-    );
-  }
+  ngOnInit() {}
 }
